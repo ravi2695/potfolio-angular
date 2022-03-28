2cc0cc81a025edcc33d736005577f90137e71672
ravi2695 <ravi.sharma2695@gmail.com>
2022-03-28 12:31:45 +0530

    main  commit

---
+++ src/app/profile/contact/contact.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders} from '@angular/common/http';
+import { ProfileService } from '../profile.service';
+import { SnotifyService } from 'ng-snotify';
+import { environment } from '../../../environments/environment';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+  export class ContactComponent implements OnInit {
+ 
+    model: any = {};
+    showMsg: any;
+    constructor(
+      private http: HttpClient
+    ){}
+  
+    ngOnInit() {
+       }
+       onSubmit(name, subject, email, message) {
+          this.http.post('http://localhost:8080/contactData',
+            { name: name, subject: subject, email: email, message: message },
+            {responseType: 'text'}).subscribe(
+              response => {
+                console.log(response);
+                if(response==="success"){
+                  this.showMsg=true;
+                }
+              }
+              
+            );
+        }  
+}
